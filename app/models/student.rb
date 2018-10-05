25039910ef4e7b8453236e52cbe5766e9da7d152
Heidi Engstrom <hengstrom25@gmail.com>
2018-10-05 19:42:21 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -16,7 +16,7 @@ class Student < ActiveRecord::Base
   
   def self.search(search)
     if search 
-      student = Student.all.select {|student| 
+      student = Student.all.select {|student| student.name
   end
     
 end
